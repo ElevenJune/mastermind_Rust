584363ce3d763de30d41cd0bcfb6692454243ca0
Guillaume Vande <guillaume.vande@gmail.com>
2022-03-30 15:33:34 +0200

    New version using enums for colors instead of i32
    _The colors are not represented by i32 anymore, it is now an enum called
    GamePegColor
    _Created GamePeg Struct to hold the color and the state of a peg
    _The state is a CodePeg enum (Correct, Missplaced, Wrong)
    _Separated computation (compare_hands) and check if game is finished
    (check_in_finished) in 2 functions (previously the logic of check if
    game is finished was in the main)

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,103 @@
 use rand::Rng;
 use std::io;
 
-const NUMBER_OF_PINS : i32 = 4;
+const NUMBER_OF_PEGS : usize = 4;
 const NUMBER_OF_COLORS : i32 = 6;
 const MAX_TURN : i32 = 12;
-static DEFAULT_HAND : [i32;NUMBER_OF_PINS as usize] = [0;NUMBER_OF_PINS as usize];
+//QUESTION : How to have a static default constructor for GamePeg ?
+//A call to GamePeg::default() does not copile : error[E0015]: calls in statics are limited to constant functions, tuple structs and tuple variants
+static DEFAULT_HAND : [GamePeg;NUMBER_OF_PEGS] = [GamePeg{color:GamePegColor::Unknown,state:CodePeg::Wrong};NUMBER_OF_PEGS as usize];
 
-struct Pins {
-    red: i32,
-    white: i32
+//------------------------------------------------
+
+#[derive(Copy, Clone)]
+#[derive(PartialEq, Eq)]
+#[derive(Debug)]
+enum GamePegColor {
+    White,
+    Orange,
+    Blue,
+    Red,
+    Yellow,
+    Green,
+    Unknown
 }
 
-impl Pins {
-    fn print(&self) {
-        println!("Red pins: {}, white pins: {}", self.red, self.white);
+impl GamePegColor {
+    fn from_i32(value: i32) -> GamePegColor {
+        match value {
+            0 => GamePegColor::White,
+            1 => GamePegColor::Orange,
+            2 => GamePegColor::Blue,
+            3 => GamePegColor::Red,
+            4 => GamePegColor::Yellow,
+            5 => GamePegColor::Green,
+            _ => GamePegColor::Unknown
+        }
+    }
+
+    fn from_str(value: &str) -> GamePegColor {
+        let lower_cased : String = value.to_lowercase();
+        match lower_cased.trim() {
+            "white" => GamePegColor::White,
+            "orange" => GamePegColor::Orange,
+            "blue" => GamePegColor::Blue,
+            "red" => GamePegColor::Red,
+            "yellow" => GamePegColor::Yellow,
+            "green" => GamePegColor::Green,
+            _ => GamePegColor::Unknown
+        }
     }
 }
 
+//------------------------------------------------
+
+//QUESTION : Why is this not a default Trait ?
+#[derive(Copy, Clone)]
+enum CodePeg {
+    Correct,
+    Missplaced,
+    Wrong
+}
+
+#[derive(Copy, Clone)]
+struct GamePeg {
+    color : GamePegColor,
+    state : CodePeg
+}
+
+impl Default for GamePeg {
+    fn default() -> Self {
+        GamePeg {
+            color:GamePegColor::Unknown,
+            state:CodePeg::Wrong
+        }
+    }
+}
+
+//------------------------------------------------
+
 
 fn main() {
-
     //Generate master's hand
     let mut rng = rand::thread_rng();
-    let master:[i32;NUMBER_OF_PINS as usize] = [(); NUMBER_OF_PINS as usize].map(|_| rng.gen_range(1..NUMBER_OF_COLORS+1));
+    let master:[GamePegColor;NUMBER_OF_PEGS] = [(); NUMBER_OF_PEGS].map(|_| GamePegColor::from_i32(rng.gen_range(0..NUMBER_OF_COLORS)));
     let mut count = 1;
     print_rules();
+    //print_line(master); //Debug only
 
     loop {
         println!("--- TURN n°{} ---",count);
-        let (user_hand, stop) = get_user_hand();
+        let (mut user_hand, stop) = get_user_hand();
         if stop { break;}
 
-        let pins = compare_hands(&master,&user_hand);
-        pins.print();
-        if pins.red == NUMBER_OF_PINS {
+        let same_hands = compare_hands(&master,&mut user_hand);
+        if same_hands {
             println!("You won ! The solution was :");
             print_line(master);
             break;
         }
-        if count == MAX_TURN {
+        else if count == MAX_TURN {
             println!("You lost ! The solution was :");
             print_line(master);
             break;
@@ -48,60 +107,57 @@ fn main() {
     
 }
 
-//Prints [i32,NUMBER_OF_PINS]
-fn print_line(list:[i32;NUMBER_OF_PINS as usize]) {
+//Prints [GamePegColor,NUMBER_OF_PEGS]
+fn print_line(list:[GamePegColor;NUMBER_OF_PEGS]) {
     println! ("{:?}", list);
 }
 
-//Returns user's hand as [i32;NUMBER_OF_PINS] and a stop bool that is true is the user asked to quit
-fn get_user_hand() -> ([i32;NUMBER_OF_PINS as usize],bool) {
+//Returns user's hand as [GamePeg;NUMBER_OF_PEGS] and a stop bool that is true is the user asked to quit
+fn get_user_hand() -> ([GamePeg;NUMBER_OF_PEGS],bool) {
     //Get user input
     let mut guess = String::new();
 
     loop {
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        if guess.trim().eq("quit"){
+            return (DEFAULT_HAND,true);
+        }
 
-    if guess.trim().eq("quit"){
-        return (DEFAULT_HAND,true);
-    }
+        let (user_hand, error) = parse_user_input(&guess);
 
-    let (user_hand, error) = parse_user_input(&guess);
-
-    if !error {
+        if !error {
             return (user_hand,false);
         } else {
-            println!("You should enter {} digits, seperated by a space, in the range 1 - {}",NUMBER_OF_PINS, NUMBER_OF_COLORS);
+            println!("You should enter {} colors (white, orange, blue, green, yellow or red)",NUMBER_OF_PEGS);
             println!("Type \"quit\" if you wish to stop the game");
             guess.clear();
         }
     }
 }
 
-fn parse_user_input(guess:&str) -> ([i32;NUMBER_OF_PINS as usize],bool) {
+//Parses the user hand, from a ¹str to a list of GamePeg
+fn parse_user_input(guess:&str) -> ([GamePeg;NUMBER_OF_PEGS],bool) {
     let split = guess.split(' ');
-    let mut array = [0;NUMBER_OF_PINS as usize];
+    let mut array = [GamePeg::default();NUMBER_OF_PEGS];
 
     //Parse each number, stop if there are letters or out of range numbers
     let mut i = 0;
     for s in split {
-        let num:i32 = match s.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {return (DEFAULT_HAND,true);},
-        };
-        if num <= 0 || num >NUMBER_OF_COLORS {
-            return (DEFAULT_HAND,true);
-        }
+        let peg_color : GamePegColor = GamePegColor::from_str(s);
 
-        if i >= NUMBER_OF_PINS as usize{
+        //QUESTION : When to use match ? when to use this ?
+        if peg_color == GamePegColor::Unknown {return (DEFAULT_HAND,true);}
+
+        if i >= NUMBER_OF_PEGS{
             break;
         }
-        array[i]=num;
+        array[i].color=peg_color;
         i+=1;
     }
-    let len_ok : bool = i >= NUMBER_OF_PINS as usize;
+    let len_ok : bool = i >= NUMBER_OF_PEGS;
     if len_ok {
         return (array,false)
     } else 
@@ -110,56 +166,71 @@ fn parse_user_input(guess:&str) -> ([i32;NUMBER_OF_PINS as usize],bool) {
     };
 }
 
-//red pin if good color in right spot
-//white if good color but not good spot
-//Compares the user'hand with the master's code and return a Pins struct
-fn compare_hands(master:&[i32;NUMBER_OF_PINS as usize],user:&[i32;NUMBER_OF_PINS as usize]) -> Pins{
+//Compares the user'hand with the master's code and returns true if they are the same
+//QUESTION : Is it better to have the state of a peg in the GamePeg struct ? It has to be passed as &mut, which seems more complicated
+fn compare_hands(master:&[GamePegColor;NUMBER_OF_PEGS],user:&mut[GamePeg;NUMBER_OF_PEGS]) -> bool{
     let len = master.len();
-    let mut red_pin = 0;
-    let mut white_pin = 0;
-    let mut master_treated : [i32;NUMBER_OF_PINS as usize] = [0; NUMBER_OF_PINS as usize];
-    let mut user_treated : [i32;NUMBER_OF_PINS as usize] = [0; NUMBER_OF_PINS as usize];
+    let mut master_treated : [i32;NUMBER_OF_PEGS] = [0; NUMBER_OF_PEGS];
+    let mut user_treated : [i32;NUMBER_OF_PEGS] = [0; NUMBER_OF_PEGS];
 
-    //Compute red pins
+    //Computes correct
     for i in 0..len {
-        if user[i]==master[i] && master_treated[i]==0 {
-            red_pin+=1;
+        if user[i].color==master[i] && master_treated[i]==0 {
+            user[i].state=CodePeg::Correct;
             master_treated[i]=1;
             user_treated[i]=1;
         }
     }
     
-    //Compute white pins
+    //Computes missplaced
     for i in 0..len {
         for j in 0..len{
-            if master_treated[j]==0 && user_treated[i]==0 && user[i]==master[j] {
-                white_pin+=1;
+            if master_treated[j]==0 && user_treated[i]==0 && user[i].color==master[j] {
+                user[i].state=CodePeg::Missplaced;
                 master_treated[j]=1;
                 user_treated[i]=1;
             }
-            //print_line(master_treated);
         }
     }
 
-    Pins{red:red_pin,white:white_pin}
+    return check_if_finished(user);
+}
+
+//QUESTION : seems less performant as before since I have to iterate through the list again, but is it cleaner ?
+fn check_if_finished(user:&[GamePeg;NUMBER_OF_PEGS]) -> bool{
+    let len = user.len();
+    let mut correct = 0;
+    let mut missplaced = 0;
+    for i in 0..len{
+        match user[i].state {
+            CodePeg::Correct=>correct+=1,
+            CodePeg::Missplaced=>missplaced+=1,
+            _ => {}
+        }
+    }
+    let finished = correct==NUMBER_OF_PEGS;
+    if !finished{
+        println!("N° of correct pegs: {}, n° of missplaced pegs: {}", correct, missplaced);
+    }
+    finished    
 }
 
 fn print_rules() {
     let mut format = String::new();
-    for i in 0..NUMBER_OF_PINS{
-        let num = format!("{}{}",i.to_string(), if i!=NUMBER_OF_PINS-1 {" "} else {""});
+    for i in 0..NUMBER_OF_PEGS{
+        let num = format!("white{}", if i!=NUMBER_OF_PEGS-1 {" "} else {""});
         format.push_str(&num);
     }
     println!("--- Mastermind ---");
     println!("- Rules :");
-    println!("-     Find the master's code, made up of 5 digits from 1 to 8 ");
+    println!("-     Find the master's code, made up of {} colors (white, orange, blue, green, yellow or red)",NUMBER_OF_PEGS);
     println!("-     You have 12 tries, if you fail to discover the master's code in less than 12 tries you loose");
-    println!("-     At each turn, you enter your guess in the format \"{}\" (your digits are to be seperated by a space)",format);
+    println!("-     At each turn, you enter your guess in the format \"{}\" (your colors are to be seperated by a space)",format);
     println!("-     The master will give you a hint about how close is your guess from his code with pins :");
-    println!("-         - A red pin means that one of the digit you chose is the right one and is in the right spot");
-    println!("-         - A white pin means that one of the digit you chose is present in his code, but not at the right spot");
+    println!("-         - A correct peg means that one of the peg you chose is of the right color and is in the right spot");
+    println!("-         - A missplaced peg means that one of the peg you chose is of the right color, but not at the right spot");
     println!("-     Good luck !");
-    println!("You should enter {} digits, seperated by a space, in the range 1 - {}",NUMBER_OF_PINS, NUMBER_OF_COLORS);
+    println!("You should enter {} colors (white, orange, blue, green, yellow or red)",NUMBER_OF_PEGS);
     println!("Type \"quit\" if you wish to stop the game");
     println!("Please enter your first guess :")
 }
